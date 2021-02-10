599f2d2d5507935bfe2a8be879f4b69dc7f4a7d6
CJ Gaspari <cjgaspari@users.noreply.github.com>
2021-02-10 14:17:50 -0800

    Initial Commit

---
+++ Shared/RowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RowerApp.swift
+//  Shared
+//
+//  Created by CJ Gaspari on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct RowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
